6f43405c24448832b28e5d5cb73a5bb1b126ff1f
rhit-yiny1 <89856239+rhit-yiny1@users.noreply.github.com>
2023-08-03 13:25:26 +0800

    starting

---
+++ Assets/Scripts/WebCamTextureToCloudVision.cs
@@ -0,0 +1,205 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using SimpleJSON;
+using System.Web;
+using System.Net;
+using System;
+
+public class WebCamTextureToCloudVision : MonoBehaviour {
+
+	public string url = "https://vision.googleapis.com/v1/images:annotate?key=";
+	public string apiKey = "AIzaSyB2uoWjdfv6LxtOyvoTxRLCBV94STh7IjA"; //Put your google cloud vision api key here
+	public float captureIntervalSeconds = 2.0f;
+	public int requestedWidth = 640;
+	public int requestedHeight = 480;
+	public FeatureType featureType = FeatureType.TEXT_DETECTION;
+	public int maxResults = 10;
+	public GameObject resPanel;
+	public Text responseText, responseArray; 
+	
+
+	WebCamTexture webcamTexture;
+	Texture2D texture2D;
+	Dictionary<string, string> headers;
+
+	[System.Serializable]
+	public class AnnotateImageRequests {
+		public List<AnnotateImageRequest> requests;
+	}
+
+	[System.Serializable]
+	public class AnnotateImageRequest {
+		public Image image;
+		public List<Feature> features;
+	}
+
+	[System.Serializable]
+	public class Image {
+		public string content;
+	}
+
+	[System.Serializable]
+	public class Feature {
+		public string type;
+		public int maxResults;
+	}
+
+	public enum FeatureType {
+		TYPE_UNSPECIFIED,
+		FACE_DETECTION,
+		LANDMARK_DETECTION,
+		LOGO_DETECTION,
+		LABEL_DETECTION,
+		TEXT_DETECTION,
+		SAFE_SEARCH_DETECTION,
+		IMAGE_PROPERTIES
+	}
+
+	// Use this for initialization
+	void Start () {
+		headers = new Dictionary<string, string>();
+		headers.Add("Content-Type", "application/json; charset=UTF-8");
+
+		if (apiKey == null || apiKey == "")
+			Debug.LogError("No API key. Please set your API key into the \"Web Cam Texture To Cloud Vision(Script)\" component.");
+		
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for (var i = 0; i < devices.Length; i++) {
+			Debug.Log (devices [i].name);
+		}
+		if (devices.Length > 0) {
+			webcamTexture = new WebCamTexture(devices[0].name, requestedWidth, requestedHeight);
+			Renderer r = GetComponent<Renderer> ();
+			if (r != null) {
+				Material m = r.material;
+				if (m != null) {
+					m.mainTexture = webcamTexture;
+				}
+			}
+			webcamTexture.Play();
+			StartCoroutine("Capture");
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// 	Mesh mesh = GetComponent<MeshFilter>().mesh;
+    //     Vector3[] vertices = mesh.vertices;
+    //     Vector3[] normals = mesh.normals;
+
+    //    for (var i = 0; i < vertices.Length; i++)
+    //     {
+    //         vertices[i] += normals[i] * Mathf.Sin(Time.time);
+    //     }
+
+    //    mesh.vertices = vertices;
+	}
+
+	
+
+	public string translate(string input, string from, string to)
+			{
+				var fromLanguage = from;
+				var toLanguage = to;
+				var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(input)}";
+				var webclient = new WebClient
+				{
+					Encoding = System.Text.Encoding.UTF8
+			};
+				var result = webclient.DownloadString(url);
+				try
+				{
+					result = result.Substring(4, result.IndexOf("\"", 4
+						, StringComparison.Ordinal) - 4);
+					return result;
+				}
+				catch(Exception e1)
+				{
+					return "error";
+				}
+			}
+
+	private IEnumerator Capture() {
+		while (true) {
+			if (this.apiKey == null)
+				yield return null;
+
+			yield return new WaitForSeconds(captureIntervalSeconds);
+
+			Color[] pixels = webcamTexture.GetPixels();
+			if (pixels.Length == 0)
+				yield return null;
+			if (texture2D == null || webcamTexture.width != texture2D.width || webcamTexture.height != texture2D.height) {
+				texture2D = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.RGBA32, false);
+			}
+
+			texture2D.SetPixels(pixels);
+			// texture2D.Apply(false); // Not required. Because we do not need to be uploaded it to GPU
+			byte[] jpg = texture2D.EncodeToJPG();
+			string base64 = System.Convert.ToBase64String(jpg);
+// #if UNITY_WEBGL	
+// 			Application.ExternalCall("post", this.gameObject.name, "OnSuccessFromBrowser", "OnErrorFromBrowser", this.url + this.apiKey, base64, this.featureType.ToString(), this.maxResults);
+// #else
+			
+			AnnotateImageRequests requests = new AnnotateImageRequests();
+			requests.requests = new List<AnnotateImageRequest>();
+
+			AnnotateImageRequest request = new AnnotateImageRequest();
+			request.image = new Image();
+			request.image.content = base64;
+			request.features = new List<Feature>();
+			Feature feature = new Feature();
+			feature.type = this.featureType.ToString();
+			feature.maxResults = this.maxResults;
+			request.features.Add(feature); 
+			requests.requests.Add(request);
+
+			string jsonData = JsonUtility.ToJson(requests, false);
+			if (jsonData != string.Empty) {
+				string url = this.url + this.apiKey;
+				byte[] postData = System.Text.Encoding.Default.GetBytes(jsonData);
+				using(WWW www = new WWW(url, postData, headers)) {
+					yield return www;
+					if (string.IsNullOrEmpty(www.error)) {
+						string responses = www.text.Replace("\n", "").Replace(" ", "");
+						//Debug.Log(responses);
+						//Console.WriteLine(responses + "\n");
+						JSONNode res = JSON.Parse(responses);
+						//string fullTextPreTrans = res["responses"][0]["textAnnotations"][0]["description"].ToString().Trim('"');
+						string fullText = res["responses"][0]["textAnnotations"][0]["boundingPoly"].ToString().Trim('"');
+						if (fullText != ""){
+							//string fullText = translate(fullTextPreTrans, "zh-CN", "en");
+							Debug.Log("OCR Response: " + fullText);
+							resPanel.SetActive(true);
+							responseText.text = fullText.Replace("\\n", " ");
+							fullText = fullText.Replace("\\n", ";");
+							string[] texts = fullText.Split(';');
+							responseArray.text = "";
+							for(int i=0;i<texts.Length;i++){
+								responseArray.text += texts[i];
+								if(i != texts.Length - 1)
+									responseArray.text += ", ";
+							}
+						}
+					} else {
+						Debug.Log("Error: " + www.error);
+					}
+				}
+			}
+// #endif
+		}
+	}
+
+#if UNITY_WEBGL
+	void OnSuccessFromBrowser(string jsonString) {
+		Debug.Log(jsonString);	
+	}
+
+	void OnErrorFromBrowser(string jsonString) {
+		Debug.Log(jsonString);	
+	}
+#endif
+
+}
---
+++ Assets/Scripts/polygonScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class polygonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateSquarePolygon();
+    }
+
+    // Update is called once per frame
+    void CreateSquarePolygon()
+    {
+        // Define the vertices of the square
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(0, 0, 0);        // Bottom-left vertex
+        vertices[1] = new Vector3(1, 0, 0);        // Bottom-right vertex
+        vertices[2] = new Vector3(1, 1, 0);        // Top-right vertex
+        vertices[3] = new Vector3(0, 1, 0);        // Top-left vertex
+
+        // Define the triangles of the square
+        int[] triangles = new int[6];
+        triangles[0] = 0;   // Bottom-left
+        triangles[1] = 1;   // Bottom-right
+        triangles[2] = 2;   // Top-right
+
+        triangles[3] = 0;   // Bottom-left
+        triangles[4] = 2;   // Top-right
+        triangles[5] = 3;   // Top-left
+
+        // Create the mesh and set its properties
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        // Attach the mesh to a new MeshRenderer
+        GameObject polygon = new GameObject("Polygon");
+        MeshFilter meshFilter = polygon.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = polygon.AddComponent<MeshRenderer>();
+        meshFilter.mesh = mesh;
+
+        // You can set the material for the polygon here
+        // Example:
+        // meshRenderer.material = new Material(Shader.Find("Standard"));
+
+        // Set the polygon's position (optional)
+        polygon.transform.position = new Vector3(0, 0, 0);
+    }
+}
